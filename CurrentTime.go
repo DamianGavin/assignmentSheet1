84772580ea5c0fec957f18d85b3cacf1adb7af89
Damian Gavin <10007969@gmit.ie>
2017-09-20 15:00:43 +0100

    Initial commit

---
+++ CurrentTime.go
@@ -0,0 +1,14 @@
+//Damian Gavin 14/09/2017
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main(){
+    fmt.Println("Welcome to my 2nd go program")
+
+	fmt.Println("The time is now", time.Now())
+}
---
+++ higherLower.go
@@ -0,0 +1,11 @@
+//Damian Gavin 20/09/17
+//program to guess a secret number
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Please guess a number between 1 and 100")
+
+}
