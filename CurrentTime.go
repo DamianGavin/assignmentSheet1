592bd27ef7bdbf25d91337e8be6820f1452a3d88
Damian Gavin <10007969@gmit.ie>
2017-09-20 15:02:50 +0100

    added comment

--- CurrentTime.go
+++ CurrentTime.go
@@ -3,12 +3,13 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
-func main(){
-    fmt.Println("Welcome to my 2nd go program")
+//comment
+func main() {
+	fmt.Println("Welcome to my 2nd go program")
 
 	fmt.Println("The time is now", time.Now())
 }
