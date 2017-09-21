802ac6ded2d5817d5c62eab7cdea07c0e43b6834
Damian Gavin <10007969@gmit.ie>
2017-09-21 15:22:58 +0100

    newer

--- higherLower.go
+++ higherLower.go
@@ -6,107 +6,106 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"bufio"
-	"strconv"
+	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 )
 
-func askInt(message string) int{
+func askInt(message string) int {
 	scanner := bufio.NewScanner(os.Stdin)
-	for{
+	for {
 		fmt.Printf("%s\n>>", message)
 		scanner.Scan()
 		s := scanner.Text()
-		n , err := strconv.Atoi(s)
+		n, err := strconv.Atoi(s)
 
-		if err == nil{
+		if err == nil {
 			return n
 		}
 		fmt.Printf("Error:'%s' is not a number\n", s)
 	}
 }
 
-func askYesHigherLower(message string)int {
+func askYesHigherLower(message string) int {
 	scanner := bufio.NewScanner(os.Stdin)
-	for{
+	for {
 		fmt.Printf("%s\n>>", message)
 		scanner.Scan()
 		s := scanner.Text()
 
-		if s == "yes"{
+		if s == "yes" {
 			return 0
 		}
-		if s == "higher"{
+		if s == "higher" {
 			return 1
 		}
-		if s == "lower"{
+		if s == "lower" {
 			return -1
 		}
 		fmt.Printf("Please only enter, 'yes', 'higher', or 'lower'\n")
 	}
 }
 
-func printToN(base int, top int){
-	for i := base; i <= top; i++{
+func printToN(base int, top int) {
+	for i := base; i <= top; i++ {
 		fmt.Println(i)
 	}
 }
 
-func keeper(){
-	
+func keeper() {
+
 	var answer int = rand.Intn(100)
 
 	fmt.Println("I'm thinking of a number")
-	for{
+	for {
 		g := askInt("Take a guess")
-		if g == answer{
+		if g == answer {
 			fmt.Println("You got it!")
 			return
 		}
-		if g < answer{
+		if g < answer {
 			fmt.Println("Nope: Higher")
-		}else{
+		} else {
 			fmt.Println("Nope: Lower")
 		}
 	}
 }
 
 //ask the user to think of a number
-func seeker(){
+func seeker() {
 	askYesHigherLower("Say 'yes' when you think of a number between 1 and 100")
-//loop to keep guessing until we find the answer
+	//loop to keep guessing until we find the answer
 
 	highest := 100
 	lowest := 1
-	for guesses := 0; guesses < 10; guesses ++{
+	for guesses := 0; guesses < 10; guesses++ {
 
 		mid := lowest
-		if highest > lowest{
-		mid = rand.Intn(highest - lowest) + lowest
+		if highest > lowest {
+			mid = rand.Intn(highest-lowest) + lowest
 		}
 		res := askYesHigherLower("Is it " + strconv.Itoa(mid) +
-		"? - yes, lower or higher?")
-		if res == 0{
+			"? - yes, lower or higher?")
+		if res == 0 {
 			fmt.Println("Yay")
 			return
 		}
-		if res > 0{
+		if res > 0 {
 			lowest = mid + 1
 		}
-		if res < 0{
+		if res < 0 {
 			highest = mid - 1
 		}
-		if lowest > highest{
-			fmt.Printf("Don't be cheating")
+		if lowest > highest {
+			fmt.Printf("Don't be fucking cheating")
 		}
 	}
 	fmt.Println("Too many guesses!")
 }
-	
 
-func main(){
+func main() {
 	//printToN(22,29)
 	seeker()
 	keeper()
